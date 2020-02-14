5a28a450630e6b6c247f64d6866d2ae0353b9731
hsiaomartin <Uioads39@gmail.com>
2020-02-14 10:44:51 +0800

    display ramdom and sin chart

--- WindowsFormsApp2/Form1.Designer.cs
+++ WindowsFormsApp2/Form1.Designer.cs
@@ -28,57 +28,103 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea3 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend3 = new System.Windows.Forms.DataVisualization.Charting.Legend();
-            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
             this.label1 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.rb1 = new System.Windows.Forms.RadioButton();
+            this.rb2 = new System.Windows.Forms.RadioButton();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.btnStop = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
             this.SuspendLayout();
             // 
             // chart1
             // 
-            chartArea3.Name = "ChartArea1";
-            this.chart1.ChartAreas.Add(chartArea3);
-            legend3.Name = "Legend1";
-            this.chart1.Legends.Add(legend3);
-            this.chart1.Location = new System.Drawing.Point(71, 58);
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(2, 12);
             this.chart1.Name = "chart1";
-            series3.ChartArea = "ChartArea1";
-            series3.Legend = "Legend1";
-            series3.Name = "Series1";
-            this.chart1.Series.Add(series3);
-            this.chart1.Size = new System.Drawing.Size(560, 300);
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(786, 346);
             this.chart1.TabIndex = 2;
             this.chart1.Text = "myChart";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(299, 365);
+            this.label1.Location = new System.Drawing.Point(667, 337);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(46, 12);
+            this.label1.Size = new System.Drawing.Size(11, 12);
             this.label1.TabIndex = 3;
-            this.label1.Text = "myLabel";
+            this.label1.Text = "0";
+            this.label1.Click += new System.EventHandler(this.labelChange);
             // 
-            // button1
+            // btnStart
             // 
-            this.button1.Location = new System.Drawing.Point(527, 365);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 4;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.myButtonClicked);
+            this.btnStart.Location = new System.Drawing.Point(253, 415);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 4;
+            this.btnStart.Text = "Time Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.myButtonClicked);
+            // 
+            // rb1
+            // 
+            this.rb1.AutoSize = true;
+            this.rb1.Checked = true;
+            this.rb1.Location = new System.Drawing.Point(481, 418);
+            this.rb1.Name = "rb1";
+            this.rb1.Size = new System.Drawing.Size(62, 16);
+            this.rb1.TabIndex = 5;
+            this.rb1.TabStop = true;
+            this.rb1.Text = "ramdom";
+            this.rb1.UseVisualStyleBackColor = true;
+            // 
+            // rb2
+            // 
+            this.rb2.AutoSize = true;
+            this.rb2.Location = new System.Drawing.Point(572, 418);
+            this.rb2.Name = "rb2";
+            this.rb2.Size = new System.Drawing.Size(36, 16);
+            this.rb2.TabIndex = 6;
+            this.rb2.TabStop = true;
+            this.rb2.Text = "sin";
+            this.rb2.UseVisualStyleBackColor = true;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(334, 415);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(75, 23);
+            this.btnStop.TabIndex = 7;
+            this.btnStop.Text = "Time Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.rb2);
+            this.Controls.Add(this.rb1);
+            this.Controls.Add(this.btnStart);
             this.Controls.Add(this.chart1);
             this.Name = "Form1";
             this.Text = "myForm";
@@ -91,7 +137,12 @@
         #endregion
         private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.RadioButton rb1;
+        private System.Windows.Forms.RadioButton rb2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button btnStop;
+
     }
 }
 
--- WindowsFormsApp2/Form1.cs
+++ WindowsFormsApp2/Form1.cs
@@ -15,17 +15,40 @@ namespace WindowsFormsApp2
         private Queue<double> dataQueue = new Queue<double>(100);
 
         private int curValue = 0;
-
+        private int label_counter = 0;
         private int num = 5;//每次刪除增加幾個點
         public Form1()
         {
             InitializeComponent();
+            InitChart();
         }
         private void myButtonClicked(object sender, EventArgs e)
         {
-            InitChart();
+            this.timer1.Start();
 
         }
+        private void labelChange(object sender, EventArgs e) {   //label click event
+            label_counter += 1;
+         //  label1. Text = label_counter.ToString();
+          //  label1.Text = this.timer1.Interval.ToString();
+        }
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            this.timer1.Stop();
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            UpdateQueueValue();
+            this.chart1.Series[0].Points.Clear();
+            for (int i = 0; i < dataQueue.Count; i++)
+            {
+                this.chart1.Series[0].Points.AddXY((i + 1), dataQueue.ElementAt(i));
+
+            }
+                label_counter += this.timer1.Interval;
+                label1.Text = label_counter.ToString() + "ms";            
+        }
+
         private void InitChart()
         {
             //定義圖表區域
@@ -36,23 +59,69 @@ namespace WindowsFormsApp2
             this.chart1.Series.Clear();
             Series series1 = new Series("S1");
             series1.ChartArea = "C1";
+            series1.ChartType = SeriesChartType.Line;
             this.chart1.Series.Add(series1);
             //設定圖表顯示樣式
             this.chart1.ChartAreas[0].AxisY.Minimum = 0;
             this.chart1.ChartAreas[0].AxisY.Maximum = 100;
-            this.chart1.ChartAreas[0].AxisX.Interval = 5;
+            this.chart1.ChartAreas[0].AxisX.Interval = 2;
             this.chart1.ChartAreas[0].AxisX.MajorGrid.LineColor = System.Drawing.Color.Silver;
             this.chart1.ChartAreas[0].AxisY.MajorGrid.LineColor = System.Drawing.Color.Silver;
             //設定標題
             this.chart1.Titles.Clear();
             this.chart1.Titles.Add("S01");
-            this.chart1.Titles[0].Text = "XXX顯示";
+            this.chart1.Titles[0].Text = "顯示";
             this.chart1.Titles[0].ForeColor = Color.RoyalBlue;
             this.chart1.Titles[0].Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
             //設定圖表顯示樣式
             this.chart1.Series[0].Color = Color.Red;
+            if (rb1.Checked)
+            {
+                this.chart1.Titles[0].Text = string.Format(" {0} 顯示", rb1.Text);
+                this.chart1.Series[0].ChartType = SeriesChartType.Line;
 
+            }
+            if (rb2.Checked)
+            {
+                this.chart1.Titles[0].Text = string.Format(" {0} 顯示", rb2.Text);
+                this.chart1.Series[0].ChartType = SeriesChartType.Spline;
+            }
             this.chart1.Series[0].Points.Clear();
+        }
+
+        private void UpdateQueueValue()
+        {
+
+            if (dataQueue.Count > 100)
+            {
+                //先出列
+                for (int i = 0; i < num; i++)
+                {
+                    dataQueue.Dequeue();
+                }
+            }
+            if (rb1.Checked)
+            {
+                this.chart1.Titles[0].Text = string.Format(" {0} 顯示", rb1.Text);
+                Random r = new Random();
+                for (int i = 0; i < num; i++)
+                {
+                    dataQueue.Enqueue(r.Next(0, 100));
+                }
+
+            }
+            if (rb2.Checked)
+            {
+                this.chart1.Titles[0].Text = string.Format(" {0} 顯示", rb2.Text);
+                for (int i = 0; i < num; i++)
+                {
+                    //對curValue只取[0,360]之間的值
+                    curValue = curValue % 360;
+                    //對得到的正弦值，放大50倍，並上移50
+                    dataQueue.Enqueue((50 * Math.Sin(curValue * Math.PI / 180)) + 50);
+                    curValue = curValue + 10;
+                }
+            }
         }
 
 
