5ecda44e828049f89a826a15a7c642f8a2c2dd6e
hsiaomartin <Uioads39@gmail.com>
2020-02-20 13:40:18 +0800

    Add special chart ,need fix.

--- WindowsFormsApp2/Form1.Designer.cs
+++ WindowsFormsApp2/Form1.Designer.cs
@@ -29,65 +29,67 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
-            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
             this.label1 = new System.Windows.Forms.Label();
-            this.btnStart = new System.Windows.Forms.Button();
+            this.myBtn = new System.Windows.Forms.Button();
             this.rb1 = new System.Windows.Forms.RadioButton();
             this.rb2 = new System.Windows.Forms.RadioButton();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
-            this.btnStop = new System.Windows.Forms.Button();
             this.mySerialPort = new System.IO.Ports.SerialPort(this.components);
             this.serialPort_textBox = new System.Windows.Forms.TextBox();
             this.serialPort_button = new System.Windows.Forms.Button();
             this.temp_radioButton = new System.Windows.Forms.RadioButton();
             this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.cartesianChart1 = new LiveCharts.WinForms.CartesianChart();
             ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
             this.SuspendLayout();
             // 
             // chart1
             // 
-            chartArea1.Name = "ChartArea1";
-            this.chart1.ChartAreas.Add(chartArea1);
-            legend1.Name = "Legend1";
-            this.chart1.Legends.Add(legend1);
+            chartArea2.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea2);
+            this.chart1.Cursor = System.Windows.Forms.Cursors.Cross;
+            legend2.Name = "Legend1";
+            this.chart1.Legends.Add(legend2);
             this.chart1.Location = new System.Drawing.Point(2, 12);
             this.chart1.Name = "chart1";
-            series1.ChartArea = "ChartArea1";
-            series1.Legend = "Legend1";
-            series1.Name = "Series1";
-            this.chart1.Series.Add(series1);
-            this.chart1.Size = new System.Drawing.Size(786, 346);
+            series2.ChartArea = "ChartArea1";
+            series2.IsValueShownAsLabel = true;
+            series2.Legend = "Legend1";
+            series2.Name = "Series1";
+            this.chart1.Series.Add(series2);
+            this.chart1.Size = new System.Drawing.Size(579, 346);
             this.chart1.TabIndex = 2;
             this.chart1.Text = "myChart";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(667, 337);
+            this.label1.Location = new System.Drawing.Point(338, 361);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(11, 12);
             this.label1.TabIndex = 3;
             this.label1.Text = "0";
             this.label1.Click += new System.EventHandler(this.labelChange);
             // 
-            // btnStart
+            // myBtn
             // 
-            this.btnStart.Location = new System.Drawing.Point(304, 415);
-            this.btnStart.Name = "btnStart";
-            this.btnStart.Size = new System.Drawing.Size(75, 23);
-            this.btnStart.TabIndex = 4;
-            this.btnStart.Text = "Time Start";
-            this.btnStart.UseVisualStyleBackColor = true;
-            this.btnStart.Click += new System.EventHandler(this.myButtonClicked);
+            this.myBtn.Location = new System.Drawing.Point(340, 415);
+            this.myBtn.Name = "myBtn";
+            this.myBtn.Size = new System.Drawing.Size(75, 23);
+            this.myBtn.TabIndex = 4;
+            this.myBtn.Text = "Time Start";
+            this.myBtn.UseVisualStyleBackColor = true;
+            this.myBtn.Click += new System.EventHandler(this.myButtonClicked);
             // 
             // rb1
             // 
             this.rb1.AutoSize = true;
             this.rb1.Checked = true;
-            this.rb1.Location = new System.Drawing.Point(481, 418);
+            this.rb1.Location = new System.Drawing.Point(481, 376);
             this.rb1.Name = "rb1";
             this.rb1.Size = new System.Drawing.Size(62, 16);
             this.rb1.TabIndex = 5;
@@ -98,7 +100,7 @@
             // rb2
             // 
             this.rb2.AutoSize = true;
-            this.rb2.Location = new System.Drawing.Point(572, 418);
+            this.rb2.Location = new System.Drawing.Point(481, 398);
             this.rb2.Name = "rb2";
             this.rb2.Size = new System.Drawing.Size(36, 16);
             this.rb2.TabIndex = 6;
@@ -110,20 +112,9 @@
             // 
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
-            // btnStop
-            // 
-            this.btnStop.Location = new System.Drawing.Point(385, 415);
-            this.btnStop.Name = "btnStop";
-            this.btnStop.Size = new System.Drawing.Size(75, 23);
-            this.btnStop.TabIndex = 7;
-            this.btnStop.Text = "Time Stop";
-            this.btnStop.UseVisualStyleBackColor = true;
-            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
-            // 
             // mySerialPort
             // 
             this.mySerialPort.PortName = "COM8";
-           // this.mySerialPort.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.mySerialPort_DataReceived);
             // 
             // serialPort_textBox
             // 
@@ -148,7 +139,7 @@
             // 
             this.temp_radioButton.AutoSize = true;
             this.temp_radioButton.Enabled = false;
-            this.temp_radioButton.Location = new System.Drawing.Point(640, 418);
+            this.temp_radioButton.Location = new System.Drawing.Point(481, 420);
             this.temp_radioButton.Name = "temp_radioButton";
             this.temp_radioButton.Size = new System.Drawing.Size(82, 16);
             this.temp_radioButton.TabIndex = 10;
@@ -160,19 +151,27 @@
             // 
             this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
             // 
+            // cartesianChart1
+            // 
+            this.cartesianChart1.Location = new System.Drawing.Point(587, 12);
+            this.cartesianChart1.Name = "cartesianChart1";
+            this.cartesianChart1.Size = new System.Drawing.Size(687, 346);
+            this.cartesianChart1.TabIndex = 11;
+            this.cartesianChart1.Text = "cartesianChart1";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1329, 450);
+            this.Controls.Add(this.cartesianChart1);
             this.Controls.Add(this.temp_radioButton);
             this.Controls.Add(this.serialPort_button);
             this.Controls.Add(this.serialPort_textBox);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.btnStop);
             this.Controls.Add(this.rb2);
             this.Controls.Add(this.rb1);
-            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.myBtn);
             this.Controls.Add(this.chart1);
             this.Name = "Form1";
             this.Text = "myForm";
@@ -185,16 +184,16 @@
         #endregion
         private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button myBtn;
         private System.Windows.Forms.RadioButton rb1;
         private System.Windows.Forms.RadioButton rb2;
         private System.Windows.Forms.Timer timer1;
-        private System.Windows.Forms.Button btnStop;
         private System.IO.Ports.SerialPort mySerialPort;
         private System.Windows.Forms.TextBox serialPort_textBox;
         private System.Windows.Forms.Button serialPort_button;
         private System.Windows.Forms.RadioButton temp_radioButton;
         private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private LiveCharts.WinForms.CartesianChart cartesianChart1;
     }
 }
 
--- WindowsFormsApp2/Form1.cs
+++ WindowsFormsApp2/Form1.cs
@@ -8,26 +8,46 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Windows.Forms.DataVisualization.Charting;
+using System.Text.RegularExpressions;
+using LiveCharts;
+using LiveCharts.Wpf;
 
 namespace WindowsFormsApp2
 {
     public partial class Form1 : Form
     {
-        private Queue<double> dataQueue = new Queue<double>(100);
+        private Queue<double> dataQueue = new Queue<double>(10);
         private bool serialPort_is_open = false;
+        private bool time_tick_is_open = false;
         private int curValue = 0;
         private int label_counter = 0;
-        private int num = 5;//每次刪除增加幾個點
-        delegate void runCode();
+        private int num = 1;//每次刪除增加幾個點
+        LineSeries myLineSeries;
+        ChartValues<double> Values = new ChartValues<double> { };
+        // delegate void runCode();
         public Form1()
         {
             InitializeComponent();
             InitChart();
-
+            CustomizedLineSeries_Load();
         }
         private void myButtonClicked(object sender, EventArgs e)
         {
-            this.timer1.Start();
+            if (!time_tick_is_open)
+            {
+                this.timer1.Start();
+                myBtn.Text = "Time Stop";
+                
+            }
+            else
+            {
+                this.timer1.Stop();
+                myBtn.Text = "Time Start";
+                
+                
+            }
+
+            time_tick_is_open= !time_tick_is_open;
 
         }
         private void labelChange(object sender, EventArgs e) {   //label click event
@@ -35,21 +55,22 @@ namespace WindowsFormsApp2
          //  label1. Text = label_counter.ToString();
           //  label1.Text = this.timer1.Interval.ToString();
         }
-        private void btnStop_Click(object sender, EventArgs e)
-        {
-            this.timer1.Stop();
-        }
         private void timer1_Tick(object sender, EventArgs e)
         {
             this.timer1.Stop();
             
             UpdateQueueValue();
             this.chart1.Series[0].Points.Clear();
+            Values.Clear();
+            
             for (int i = 0; i < dataQueue.Count; i++)
             {
                 this.chart1.Series[0].Points.AddXY((i + 1), dataQueue.ElementAt(i));
-
+                Values.Add(dataQueue.ElementAt(i));
+                
+                
             }
+                myLineSeries.Values = Values;
                 label_counter += this.timer1.Interval;
                 label1.Text = label_counter.ToString() + "ms";
             this.timer1.Start();
@@ -59,16 +80,17 @@ namespace WindowsFormsApp2
         {
             //定義圖表區域
             this.chart1.ChartAreas.Clear();
-            ChartArea chartArea1 = new ChartArea("C1");
+            ChartArea chartArea1 = new ChartArea("ChartArea 1");
             this.chart1.ChartAreas.Add(chartArea1);
             //定義儲存和顯示點的容器
             this.chart1.Series.Clear();
-            Series series1 = new Series("S1");
-            series1.ChartArea = "C1";
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series("Series 1");
+            series1.ChartArea = "ChartArea 1";
             series1.ChartType = SeriesChartType.Line;
+            series1.IsValueShownAsLabel = true;
             this.chart1.Series.Add(series1);
             //設定圖表顯示樣式
-            this.chart1.ChartAreas[0].AxisY.Minimum = 15;
+            this.chart1.ChartAreas[0].AxisY.Minimum = 17;
             this.chart1.ChartAreas[0].AxisY.Maximum = 30;
             this.chart1.ChartAreas[0].AxisX.Interval = 1;
             this.chart1.ChartAreas[0].AxisX.MajorGrid.LineColor = System.Drawing.Color.Silver;
@@ -81,6 +103,7 @@ namespace WindowsFormsApp2
             this.chart1.Titles[0].Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
             //設定圖表顯示樣式
             this.chart1.Series[0].Color = Color.Red;
+
             if (rb1.Checked)
             {
                 this.chart1.Titles[0].Text = string.Format(" {0} 顯示", rb1.Text);
@@ -98,7 +121,7 @@ namespace WindowsFormsApp2
         private void UpdateQueueValue()
         {
             
-            if (dataQueue.Count > 20-6)
+            if (dataQueue.Count > 20)
             {
                 //先出列
                 for (int i = 0; i < num; i++)
@@ -112,7 +135,7 @@ namespace WindowsFormsApp2
                 Random r = new Random();
                 for (int i = 0; i < num; i++)
                 {
-                    dataQueue.Enqueue(r.Next(0, 100));
+                    dataQueue.Enqueue(r.Next(20, 30));
                 }
 
             }
@@ -124,7 +147,7 @@ namespace WindowsFormsApp2
                     //對curValue只取[0,360]之間的值
                     curValue = curValue % 360;
                     //對得到的正弦值，放大50倍，並上移50
-                    dataQueue.Enqueue((50 * Math.Sin(curValue * Math.PI / 180)) + 50);
+                    dataQueue.Enqueue((10 * Math.Sin(curValue * Math.PI / 180)) + 10);
                     curValue = curValue + 10;
                 }
             }
@@ -135,20 +158,20 @@ namespace WindowsFormsApp2
             if (temp_radioButton.Checked) {
                 this.chart1.Titles[0].Text = string.Format("{0}顯示","溫度");
                 string indata = mySerialPort.ReadLine();
-                // string RegularExpressions = "^(([0-9]+\\.[0-9]*[1-9][0-9]*)|([0-9]*[1-9][0-9]*\\.[0-9]+)|([0-9]*[1-9][0-9]*))$";
-                float indata_r=0;
+                double indata_r=0;
                // backgroundWorker1.RunWorkerAsync(indata);
                 Invoke(new Action<string>(
                     (s) =>
                     {
-                       
-                     //   if (indata == RegularExpressions)
-                      //  {
-                            indata_r = float.Parse(s);
+
+                           if (Regex.IsMatch(s, RegularExp.UnMinusFloat))
+                          {
+                            
+                            indata_r = Math.Round(double.Parse(s),2);
                             serialPort_textBox.Text += s + " °C\r\n";
                             serialPort_textBox.SelectionStart = serialPort_textBox.Text.Length;
                             serialPort_textBox.ScrollToCaret();
-                        //  }
+                          }
                     }
                     ), indata);
                     for (int i = 0; i < num; i++)
@@ -210,12 +233,67 @@ namespace WindowsFormsApp2
                 
         }
 
+
+
+
         private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
         {
 
         }
+
+
+        private void CustomizedLineSeries_Load()
+        {
+            myLineSeries = new LineSeries() { 
+                Values = new ChartValues<double> { 5, 3, 5, 7, 3, 9 },
+                StrokeThickness = 2,
+                Stroke = new System.Windows.Media.SolidColorBrush(System.Windows.Media.Color.FromRgb(28, 142, 196)),
+                Fill = System.Windows.Media.Brushes.Transparent,
+                LineSmoothness = 1,
+                PointGeometrySize = 15,
+                PointForeground =
+                    new System.Windows.Media.SolidColorBrush(System.Windows.Media.Color.FromRgb(34, 46, 49))
+            };
+            
+            cartesianChart1.Series.Add(myLineSeries);
+
+            cartesianChart1.Background = new System.Windows.Media.SolidColorBrush(System.Windows.Media.Color.FromRgb(34, 46, 49));          
+        }
+
     }
 
 
 
+}
+
+
+public struct RegularExp
+{
+    public const string Chinese = @"^[\u4E00-\u9FA5\uF900-\uFA2D]+$";
+    public const string Color = "^#[a-fA-F0-9]{6}";
+    public const string Date = @"^((((1[6-9]|[2-9]\d)\d{2})-(0?[13578]|1[02])-(0?[1-9]|[12]\d|3[01]))|(((1[6-9]|[2-9]\d)\d{2})-(0?[13456789]|1[012])-(0?[1-9]|[12]\d|30))|(((1[6-9]|[2-9]\d)\d{2})-0?2-(0?[1-9]|1\d|2[0-8]))|(((1[6-9]|[2-9]\d)(0[48]|[2468][048]|[13579][26])|((16|[2468][048]|[3579][26])00))-0?2-29-))$";
+    public const string DateTime = @"^((((1[6-9]|[2-9]\d)\d{2})-(0?[13578]|1[02])-(0?[1-9]|[12]\d|3[01]))|(((1[6-9]|[2-9]\d)\d{2})-(0?[13456789]|1[012])-(0?[1-9]|[12]\d|30))|(((1[6-9]|[2-9]\d)\d{2})-0?2-(0?[1-9]|1\d|2[0-8]))|(((1[6-9]|[2-9]\d)(0[48]|[2468][048]|[13579][26])|((16|[2468][048]|[3579][26])00))-0?2-29-)) (20|21|22|23|[0-1]?\d):[0-5]?\d:[0-5]?\d$";
+    public const string Email = @"^[\w-]+(\.[\w-]+)*@[\w-]+(\.[\w-]+)+$";
+    public const string Float = @"^(-?\d+)(\.\d+)?$";
+    public const string ImageFormat = @"\.(?i:jpg|bmp|gif|ico|pcx|jpeg|tif|png|raw|tga)$";
+    public const string Integer = @"^-?\d+$";
+    public const string IP = @"^(\d{1,2}|1\d\d|2[0-4]\d|25[0-5])\.(\d{1,2}|1\d\d|2[0-4]\d|25[0-5])\.(\d{1,2}|1\d\d|2[0-4]\d|25[0-5])\.(\d{1,2}|1\d\d|2[0-4]\d|25[0-5])$";
+    public const string Letter = "^[A-Za-z]+$";
+    public const string LowerLetter = "^[a-z]+$";
+    public const string MinusFloat = @"^(-(([0-9]+\.[0-9]*[1-9][0-9]*)|([0-9]*[1-9][0-9]*\.[0-9]+)|([0-9]*[1-9][0-9]*)))$";
+    public const string MinusInteger = "^-[0-9]*[1-9][0-9]*$";
+    public const string Mobile = "^0{0,1}13[0-9]{9}$";
+    public const string NumbericOrLetterOrChinese = @"^[A-Za-z0-9\u4E00-\u9FA5\uF900-\uFA2D]+$";
+    public const string Numeric = "^[0-9]+$";
+    public const string NumericOrLetter = "^[A-Za-z0-9]+$";
+    public const string NumericOrLetterOrUnderline = @"^\w+$";
+    public const string PlusFloat = @"^(([0-9]+\.[0-9]*[1-9][0-9]*)|([0-9]*[1-9][0-9]*\.[0-9]+)|([0-9]*[1-9][0-9]*))$";
+    public const string PlusInteger = "^[0-9]*[1-9][0-9]*$";
+    public const string Telephone = @"(\d+-)?(\d{4}-?\d{7}|\d{3}-?\d{8}|^\d{7,8})(-\d+)?";
+    public const string UnMinusFloat = @"^\d+(\.\d+)?$";
+    public const string UnMinusInteger = @"\d+$";
+    public const string UnPlusFloat = @"^((-\d+(\.\d+)?)|(0+(\.0+)?))$";
+    public const string UnPlusInteger = @"^((-\d+)|(0+))$";
+    public const string UpperLetter = "^[A-Z]+$";
+    public const string Url = @"^http(s)?://([\w-]+\.)+[\w-]+(/[\w- ./?%&=]*)?$";
 }
